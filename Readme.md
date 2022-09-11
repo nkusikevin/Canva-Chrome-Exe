d9b8f7a257088e611a880c738d2694157f2cb664
nkusikevin <nkusikvnhart@gmail.com>
2022-09-11 20:33:31 +0200

    adding sign-up form

--- Readme.md
+++ Readme.md
@@ -5,5 +5,3 @@
 - [x] Webpack Compatible
 - [x] TailwindCSS 3.0 Compatible
 
-Subscribe to my youtube channel
-https://bit.ly/manshu
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import '../assets/tailwind.css'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import "../assets/tailwind.css";
+import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
+import App from "../App";
 
-const test = (
-    <h1 className="text-3xl text-green-500 font-bold underline">
-        Hello world!
-    </h1>
-)
+const test = <h1 className='text-3xl text-green-500 font-bold underline'></h1>;
 
-const container = document.createElement('div')
-document.body.appendChild(container)
-const root = createRoot(container)
-root.render(test)
+
+const container = document.createElement("div");
+container.setAttribute("id", "root");
+document.body.appendChild(container);
+
+
+// First, we create a root
+const root = createRoot(document.querySelector('#root'));
+
+// Initial render. Container is implicitly accessed.
+root.render(<App />);
+
+
+
+
+
+
